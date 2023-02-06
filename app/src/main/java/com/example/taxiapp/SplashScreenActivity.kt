fd37c03308ff2a47e7e0a0c037061115c0896423
Gramzes <gramzin4666@gmail.com>
2023-02-06 13:29:22 +0300

    Сделан SplashScreen

--- app/src/main/java/com/example/taxiapp/SplashScreenActivity.kt
+++ app/src/main/java/com/example/taxiapp/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.taxiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,22 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val thread = object :Thread(){
+            override fun run() {
+                try{
+                    sleep(2000)
+                }
+                catch (ex: Exception){
+                    ex.printStackTrace()
+                }
+                finally {
+                    val intent = Intent(this@SplashScreenActivity, SignInActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+        thread.start()
     }
 }
